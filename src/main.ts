f1618c22a5b264ef67a4da6289e048774caef0be
Alabs02 <alabson.inc@gmail.com>
2023-05-19 15:10:10 +0100

    Added Prettier for code formatting

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+
+// APP
+import App from '@/App.vue';
+
+// ROUTER
+import router from '@/router';
+
+// STORE
+import store from '@/store';
+
+// GLOBAL STYLES
+// import '@/styles/globals.scss'
 
 createApp(App).use(store).use(router).mount('#app');
